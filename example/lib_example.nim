cb55af4c6ccfb226a149c5bf335e270c4422bd30
Clement <clement.junca@gmail.com>
2014-03-30 23:21:07 +0200

    Config structures adapted

--- example/lib_example.nim
+++ example/lib_example.nim
@@ -1,9 +1,9 @@
 import dimrod
 
 const 
-    config_unit : TBasicUnitsConf = (@["m", "kg", "s"],@[(-1,2), (-1,1), (-2,2)])
+    config_unit : TBasicUnitsConf = @[("m", (-1,2)), ("kg",(-1,1)), ("s", (-2,2))]
     uname_config = ("T", "v", "nodim")
-    alias_config : TAliasConf= (@["N", "Pa", "J"], @[@[1,1,-2], @[-1,1,-2], @[2,1,-2]])
+    alias_config : TAliasConf= @[("N",@[1,1,-2]), ("Pa",@[-1,1,-2]), ("J",@[2,1,-2])]
 
 
 init_unit(config_unit, uname_config, alias_config)
